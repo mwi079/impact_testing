85a425a0bf21b9c28f83535cf169c3869e74f807
Malcolm Wilkie <malcolm.wilkie@imprimatur.co.uk>
2022-04-01 17:27:23 +0100

    messing around with quasar

--- src/src/pages/IndexPage.vue
+++ src/src/pages/IndexPage.vue
@@ -6,44 +6,46 @@
       :todos="todos"
       :meta="meta"
     ></example-component>
+    <DummyFetch />
   </q-page>
 </template>
 
 <script lang="ts">
 import { Todo, Meta } from 'components/models';
 import ExampleComponent from 'components/ExampleComponent.vue';
+import DummyFetch from 'components/dummyFetch.vue';
 import { defineComponent, ref } from 'vue';
 
 export default defineComponent({
   name: 'IndexPage',
-  components: { ExampleComponent },
+  components: { ExampleComponent, DummyFetch },
   setup() {
     const todos = ref<Todo[]>([
       {
         id: 1,
-        content: 'ct1'
+        content: 'ct1',
       },
       {
         id: 2,
-        content: 'ct2'
+        content: 'ct2',
       },
       {
         id: 3,
-        content: 'ct3'
+        content: 'ct3',
       },
       {
         id: 4,
-        content: 'ct4'
+        content: 'ct4',
       },
       {
         id: 5,
-        content: 'ct5'
-      }
+        content: 'ct5',
+      },
     ]);
     const meta = ref<Meta>({
-      totalCount: 1200
+      totalCount: 1200,
     });
     return { todos, meta };
-  }
+  },
 });
 </script>
